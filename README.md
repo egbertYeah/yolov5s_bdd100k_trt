86af56d3228a0772bfd20c15e4ad5b7d32545a27
egbertYeah <y_18975464513@163.com>
2021-04-20 10:17:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# yolov5
+
+The Pytorch implementation is [ultralytics/yolov5](https://github.com/ultralytics/yolov5).
+
+this reposity is introduced from [tensorrtx/yolov5@1.0](https://github.com/wang-xinyu/tensorrtx/tree/yolov5-v1.0) and suitable for [yolov5-bdd100k](https://github.com/williamhyin/yolov5s_bdd100k)
+
+## How to Run
+
+```
+1. generate yolov5s-bdd.wts from pytorch implementation with yolov5s-bdd.pt
+git clone https://github.com/williamhyin/yolov5s_bdd100k.git
+// transform its weights into 'yolov5s-bdd.pt'
+cd yolov5s_bdd100k/runs/exp0_yolov5s_bdd_prew/weights/
+cp best_yolov5s_bdd_prew.pt [yolov5s_bdd100k主目录下]
+cp ./tensorrt/gen_wts.py .
+python gen_wts.py
+// a file 'yolov5s-bdd.wts' will be generated.(also provided the wts file)
+
+2. put yolov5s-bdd.wts into tensorrt, build and run
+
+mv yolov5s-bdd.wts ./tensorrt/
+cd ./tensorrt/
+mkdir build
+cd build
+cmake ..
+make
+sudo ./yolov5s -s             // serialize model to plan file i.e. 'yolov5s.engine'
+sudo ./yolov5s -d  ../samples // deserialize plan file and run inference, the images in samples will be processed.
+sudo ./yolov5s -d  ../exmaple.mp4 //eserialize plan file and run inference with video input
+
+3. check the images generated, as follows.
+input image folder = samples
+output image folder = outputs 
+if you would like to find the detection result with txt file, you can change the code in line 228 `bool save_txt = false;`
+output deteciton txt information = detection-results(class + conf + center_x + center_y + w + h)
+output video result = result.avi
+```
+[!](alt image0)(outputs/1.jpg)
+
+[!](alt image0)(outputs/2.jpg)
+
+## mAP testing
+testing mAP in tensorrt with [mAP](https://github.com/Cartucho/mAP.git)
+by move the detection-results into mAP/input/detection-results, and `pythons3 main.py`
+For details, see[mAP](https://github.com/Cartucho/mAP)
+## Config
+
+- Input shape defined in yololayer.h
+- Number of classes defined in yololayer.h
+- FP16/FP32 can be selected by the macro in yolov5s.cpp
+- GPU id can be selected by the macro in yolov5s.cpp
+- NMS thresh in yolov5s.cpp
+- BBox confidence thresh in yolov5s.cpp
+- Batch size in yolov5s.cpp
+
+## More Information
+
+See the readme in [home page.](https://github.com/wang-xinyu/tensorrtx)
+
---
+++ tensorrt/yolov5s.cpp
@@ -0,0 +1,407 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include "cuda_runtime_api.h"
+#include "logging.h"
+#include "common.hpp"
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+#define USE_FP16  // comment out this if want to use FP32
+#define DEVICE 0  // GPU id
+#define NMS_THRESH 0.5
+#define CONF_THRESH 0.4
+#define BATCH_SIZE 1
+
+// stuff we know about the network and the input/output blobs
+static const int INPUT_H = Yolo::INPUT_H;
+static const int INPUT_W = Yolo::INPUT_W;
+static const int OUTPUT_SIZE = Yolo::MAX_OUTPUT_BBOX_COUNT * sizeof(Yolo::Detection) / sizeof(float) + 1;  // we assume the yololayer outputs no more than 1000 boxes that conf >= 0.1
+const char* INPUT_BLOB_NAME = "data";
+const char* OUTPUT_BLOB_NAME = "prob";
+static Logger gLogger;
+REGISTER_TENSORRT_PLUGIN(YoloPluginCreator);
+
+// Creat the engine using only the API and not any parser.
+ICudaEngine* createEngine(unsigned int maxBatchSize, IBuilder* builder, IBuilderConfig* config, DataType dt) {
+    INetworkDefinition* network = builder->createNetworkV2(0U);
+
+    // Create input tensor of shape {3, INPUT_H, INPUT_W} with name INPUT_BLOB_NAME
+    ITensor* data = network->addInput(INPUT_BLOB_NAME, dt, Dims3{3, INPUT_H, INPUT_W});
+    assert(data);
+
+    std::map<std::string, Weights> weightMap = loadWeights("../yolov5s-bdd.wts");
+    Weights emptywts{DataType::kFLOAT, nullptr, 0};
+
+    // yolov5 backbone
+    auto focus0 = focus(network, weightMap, *data, 3, 32, 3, "model.0");
+    auto conv1 = convBnLeaky(network, weightMap, *focus0->getOutput(0), 64, 3, 2, 1, "model.1");
+    auto bottleneck_CSP2 = bottleneckCSP(network, weightMap, *conv1->getOutput(0), 64, 64, 1, true, 1, 0.5, "model.2");
+    auto conv3 = convBnLeaky(network, weightMap, *bottleneck_CSP2->getOutput(0), 128, 3, 2, 1, "model.3");
+    auto bottleneck_csp4 = bottleneckCSP(network, weightMap, *conv3->getOutput(0), 128, 128, 3, true, 1, 0.5, "model.4");
+    auto conv5 = convBnLeaky(network, weightMap, *bottleneck_csp4->getOutput(0), 256, 3, 2, 1, "model.5");
+    auto bottleneck_csp6 = bottleneckCSP(network, weightMap, *conv5->getOutput(0), 256, 256, 3, true, 1, 0.5, "model.6");
+    auto conv7 = convBnLeaky(network, weightMap, *bottleneck_csp6->getOutput(0), 512, 3, 2, 1, "model.7");
+    auto spp8 = SPP(network, weightMap, *conv7->getOutput(0), 512, 512, 5, 9, 13, "model.8");
+
+    // yolov5 head
+    auto bottleneck_csp9 = bottleneckCSP(network, weightMap, *spp8->getOutput(0), 512, 512, 1, false, 1, 0.5, "model.9");
+    auto conv10 = convBnLeaky(network, weightMap, *bottleneck_csp9->getOutput(0), 256, 1, 1, 1, "model.10");
+
+    float *deval = reinterpret_cast<float*>(malloc(sizeof(float) * 256 * 2 * 2));
+    for (int i = 0; i < 256 * 2 * 2; i++) {
+        deval[i] = 1.0;
+    }
+    Weights deconvwts11{DataType::kFLOAT, deval, 256 * 2 * 2};
+    IDeconvolutionLayer* deconv11 = network->addDeconvolutionNd(*conv10->getOutput(0), 256, DimsHW{2, 2}, deconvwts11, emptywts);
+    deconv11->setStrideNd(DimsHW{2, 2});
+    deconv11->setNbGroups(256);
+    weightMap["deconv11"] = deconvwts11;
+
+    ITensor* inputTensors12[] = {deconv11->getOutput(0), bottleneck_csp6->getOutput(0)};
+    auto cat12 = network->addConcatenation(inputTensors12, 2);
+    auto bottleneck_csp13 = bottleneckCSP(network, weightMap, *cat12->getOutput(0), 512, 256, 1, false, 1, 0.5, "model.13");
+    auto conv14 = convBnLeaky(network, weightMap, *bottleneck_csp13->getOutput(0), 128, 1, 1, 1, "model.14");
+
+    Weights deconvwts15{DataType::kFLOAT, deval, 128 * 2 * 2};
+    IDeconvolutionLayer* deconv15 = network->addDeconvolutionNd(*conv14->getOutput(0), 128, DimsHW{2, 2}, deconvwts15, emptywts);
+    deconv15->setStrideNd(DimsHW{2, 2});
+    deconv15->setNbGroups(128);
+
+    ITensor* inputTensors16[] = {deconv15->getOutput(0), bottleneck_csp4->getOutput(0)};
+    auto cat16 = network->addConcatenation(inputTensors16, 2);
+    auto bottleneck_csp17 = bottleneckCSP(network, weightMap, *cat16->getOutput(0), 256, 128, 1, false, 1, 0.5, "model.17");
+    IConvolutionLayer* conv18 = network->addConvolutionNd(*bottleneck_csp17->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.18.weight"], weightMap["model.18.bias"]);
+
+    auto conv19 = convBnLeaky(network, weightMap, *bottleneck_csp17->getOutput(0), 128, 3, 2, 1, "model.19");
+    ITensor* inputTensors20[] = {conv19->getOutput(0), conv14->getOutput(0)};
+    auto cat20 = network->addConcatenation(inputTensors20, 2);
+    auto bottleneck_csp21 = bottleneckCSP(network, weightMap, *cat20->getOutput(0), 256, 256, 1, false, 1, 0.5, "model.21");
+    IConvolutionLayer* conv22 = network->addConvolutionNd(*bottleneck_csp21->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.22.weight"], weightMap["model.22.bias"]);
+
+    auto conv23 = convBnLeaky(network, weightMap, *bottleneck_csp21->getOutput(0), 256, 3, 2, 1, "model.23");
+    ITensor* inputTensors24[] = {conv23->getOutput(0), conv10->getOutput(0)};
+    auto cat24 = network->addConcatenation(inputTensors24, 2);
+    auto bottleneck_csp25 = bottleneckCSP(network, weightMap, *cat24->getOutput(0), 512, 512, 1, false, 1, 0.5, "model.25");
+    IConvolutionLayer* conv26 = network->addConvolutionNd(*bottleneck_csp25->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["model.26.weight"], weightMap["model.26.bias"]);
+
+    auto creator = getPluginRegistry()->getPluginCreator("YoloLayer_TRT", "1");
+    const PluginFieldCollection* pluginData = creator->getFieldNames();
+    IPluginV2 *pluginObj = creator->createPlugin("yololayer", pluginData);
+    ITensor* inputTensors_yolo[] = {conv26->getOutput(0), conv22->getOutput(0), conv18->getOutput(0)};
+    auto yolo = network->addPluginV2(inputTensors_yolo, 3, *pluginObj);
+
+    yolo->getOutput(0)->setName(OUTPUT_BLOB_NAME);
+    network->markOutput(*yolo->getOutput(0));
+
+    // Build engine
+    builder->setMaxBatchSize(maxBatchSize);
+    config->setMaxWorkspaceSize(16 * (1 << 20));  // 16MB
+#ifdef USE_FP16
+    config->setFlag(BuilderFlag::kFP16);
+#endif
+    std::cout << "Building engine, please wait for a while..." << std::endl;
+    ICudaEngine* engine = builder->buildEngineWithConfig(*network, *config);
+    std::cout << "Build engine successfully!" << std::endl;
+
+    // Don't need the network any more
+    network->destroy();
+
+    // Release host memory
+    for (auto& mem : weightMap)
+    {
+        free((void*) (mem.second.values));
+    }
+
+    return engine;
+}
+
+void APIToModel(unsigned int maxBatchSize, IHostMemory** modelStream) {
+    // Create builder
+    IBuilder* builder = createInferBuilder(gLogger);
+    IBuilderConfig* config = builder->createBuilderConfig();
+
+    // Create model to populate the network, then set the outputs and create an engine
+    ICudaEngine* engine = createEngine(maxBatchSize, builder, config, DataType::kFLOAT);
+    assert(engine != nullptr);
+
+    // Serialize the engine
+    (*modelStream) = engine->serialize();
+
+    // Close everything down
+    engine->destroy();
+    builder->destroy();
+}
+
+void doInference(IExecutionContext& context, float* input, float* output, int batchSize) {
+    const ICudaEngine& engine = context.getEngine();
+
+    // Pointers to input and output device buffers to pass to engine.
+    // Engine requires exactly IEngine::getNbBindings() number of buffers.
+    assert(engine.getNbBindings() == 2);
+    void* buffers[2];
+
+    // In order to bind the buffers, we need to know the names of the input and output tensors.
+    // Note that indices are guaranteed to be less than IEngine::getNbBindings()
+    const int inputIndex = engine.getBindingIndex(INPUT_BLOB_NAME);
+    const int outputIndex = engine.getBindingIndex(OUTPUT_BLOB_NAME);
+
+    // Create GPU buffers on device
+    CHECK(cudaMalloc(&buffers[inputIndex], batchSize * 3 * INPUT_H * INPUT_W * sizeof(float)));
+    CHECK(cudaMalloc(&buffers[outputIndex], batchSize * OUTPUT_SIZE * sizeof(float)));
+
+    // Create stream
+    cudaStream_t stream;
+    CHECK(cudaStreamCreate(&stream));
+
+    // DMA input batch data to device, infer on the batch asynchronously, and DMA output back to host
+    CHECK(cudaMemcpyAsync(buffers[inputIndex], input, batchSize * 3 * INPUT_H * INPUT_W * sizeof(float), cudaMemcpyHostToDevice, stream));
+    context.enqueue(batchSize, buffers, stream, nullptr);
+    CHECK(cudaMemcpyAsync(output, buffers[outputIndex], batchSize * OUTPUT_SIZE * sizeof(float), cudaMemcpyDeviceToHost, stream));
+    cudaStreamSynchronize(stream);
+
+    // Release stream and buffers
+    cudaStreamDestroy(stream);
+    CHECK(cudaFree(buffers[inputIndex]));
+    CHECK(cudaFree(buffers[outputIndex]));
+}
+
+int main(int argc, char** argv) {
+    cudaSetDevice(DEVICE);
+    // create a model using the API directly and serialize it to a stream
+    char *trtModelStream{nullptr};
+    size_t size{0};
+
+    if (argc == 2 && std::string(argv[1]) == "-s") {
+        IHostMemory* modelStream{nullptr};
+        APIToModel(BATCH_SIZE, &modelStream);
+        assert(modelStream != nullptr);
+        std::ofstream p("yolov5s.engine", std::ios::binary);
+        if (!p) {
+            std::cerr << "could not open plan output file" << std::endl;
+            return -1;
+        }
+        p.write(reinterpret_cast<const char*>(modelStream->data()), modelStream->size());
+        modelStream->destroy();
+        return 0;
+    } else if (argc == 3 && std::string(argv[1]) == "-d") {
+        std::ifstream file("yolov5s.engine", std::ios::binary);
+        if (file.good()) {
+            file.seekg(0, file.end);
+            size = file.tellg();
+            file.seekg(0, file.beg);
+            trtModelStream = new char[size];
+            assert(trtModelStream);
+            file.read(trtModelStream, size);
+            file.close();
+        }
+    } else {
+        std::cerr << "arguments not right!" << std::endl;
+        std::cerr << "./yolov5s -s  // serialize model to plan file" << std::endl;
+        std::cerr << "./yolov5s -d ../samples  // deserialize plan file and run inference" << std::endl;
+        return -1;
+    }
+
+    // ----------------------- class type -------------------- //
+    std::vector<std::string> categories = {"person","rider","car",
+                                            "bus","truck","bike","motor","tl_green",
+                                            "tl_red","tl_yellow","tl_none","t_sign","train"};
+    // -------------------------------------------------------- //
+
+    // ---------------------prepare input data --------------------------- //
+    static float data[BATCH_SIZE * 3 * INPUT_H * INPUT_W];
+    static float prob[BATCH_SIZE * OUTPUT_SIZE];
+    IRuntime* runtime = createInferRuntime(gLogger);
+    assert(runtime != nullptr);
+    ICudaEngine* engine = runtime->deserializeCudaEngine(trtModelStream, size);
+    assert(engine != nullptr);
+    IExecutionContext* context = engine->createExecutionContext();
+    assert(context != nullptr);
+    delete[] trtModelStream;
+    // ------------------------------------------------------------------------ // 
+    bool INFERENCE_VIDEO = false;
+    if (std::string(argv[2]).find("mp4") != std::string::npos)
+    	INFERENCE_VIDEO = true;         // video input
+    if(!INFERENCE_VIDEO)
+    {
+	    bool save_txt = false;  // save detection result into txt files
+	    bool save_img = true;  // whether save the image results
+
+	    std::vector<std::string> file_names;  // filename in folder
+
+	    if (read_files_in_dir(argv[2], file_names) < 0) 
+	    {
+	        std::cout << "read_files_in_dir failed." << std::endl;
+	        return -1;
+	    }
+
+	    int fcount = 0;
+
+	    for (int f = 0; f < (int)file_names.size(); f++)
+	    {
+	        fcount++;
+	        if (fcount < BATCH_SIZE && f + 1 != (int)file_names.size()) continue;
+	        for (int b = 0; b < fcount; b++) 
+	        {
+	            cv::Mat img = cv::imread(std::string(argv[2]) + "/" + file_names[f - fcount + 1 + b]);
+	            if (img.empty()) continue;
+	            cv::Mat pr_img = preprocess_img(img);
+	            // image normalization 
+	            for (int i = 0; i < INPUT_H * INPUT_W; i++) 
+	            {
+	                data[b * 3 * INPUT_H * INPUT_W + i] = pr_img.at<cv::Vec3b>(i)[2] / 255.0;
+	                data[b * 3 * INPUT_H * INPUT_W + i + INPUT_H * INPUT_W] = pr_img.at<cv::Vec3b>(i)[1] / 255.0;
+	                data[b * 3 * INPUT_H * INPUT_W + i + 2 * INPUT_H * INPUT_W] = pr_img.at<cv::Vec3b>(i)[0] / 255.0;
+	            }
+	        }
+
+	        // Run inference
+	        auto start = std::chrono::system_clock::now();
+	        doInference(*context, data, prob, BATCH_SIZE);
+	        auto end = std::chrono::system_clock::now();
+	        std::cout << "Inference time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+
+	        std::vector<std::vector<Yolo::Detection>> batch_res(fcount);
+	        // nms
+	        for (int b = 0; b < fcount; b++) 
+	        {
+	            auto& res = batch_res[b];
+	            nms(res, &prob[b * OUTPUT_SIZE], CONF_THRESH, NMS_THRESH);
+	        }
+            
+	        for (int b = 0; b < fcount; b++)
+	        {
+                cv::Mat img;
+                cv::Rect r;
+                img = cv::imread(std::string(argv[2]) + "/" + file_names[f - fcount + 1 + b]);
+	            auto& res = batch_res[b];
+	            // inference results
+	            std::cout << "object number is: " << res.size() << std::endl;
+	            if(save_txt)
+	            {
+	            	// open file
+	            	std::string::size_type idx = file_names[f - fcount + 1 + b].find('.');
+	            	std::string txt_file = file_names[f - fcount + 1 + b].substr(0, idx) + ".txt";
+	            	std::ofstream destFile("../detection-results/" + txt_file, std::ios::out);
+	            	if(!destFile)
+	            	{
+	            		std::cout << "open file error!" << std::endl;
+	            		return 0;
+	            	}
+
+	            	for(size_t j = 0; j < res.size(); j++)
+	            	{
+	            		
+	            		std::string originalClass = categories[(int)res[j].class_id];
+	            		// class + conf + center_x + center_y + w + h -> x1 + y1 + x2 + y2
+                        r = get_rect(img, res[j].bbox);
+                        destFile << originalClass << " " 
+                                 << res[j].conf << " "
+                                 << r.tl().x<< " "
+                                 << r.tl().y<< " " 
+                                 << r.br().x<< " " 
+                                 << r.br().y << std::endl;
+	            	}
+	            	destFile.close();
+	            }
+
+	            if(save_img)
+	            {
+	            	for (size_t j = 0; j < res.size(); j++)
+                    {
+                        int class_idx = (int)res[j].class_id;
+                        cv::Rect r = get_rect(img, res[j].bbox);
+                        cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+                        cv::putText(img, categories[class_idx], cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+                                
+                    }
+		            cv::imwrite("../outputs/" + file_names[f - fcount + 1 + b], img);
+
+	            }
+	        }
+	        fcount = 0;
+	    }
+	}
+	else
+	{
+        std::cout << "video stream input !" << std::endl;
+		cv::VideoCapture video(argv[2]);
+		if(!video.isOpened())
+		{
+			std::cout << "failed to open video file!" << std::endl;
+			return -1;
+		}
+		int frame_num = video.get(cv::CAP_PROP_FRAME_COUNT);
+		std::cout << "total frame number is: " << frame_num << std::endl;
+		float fps = 0.0;
+		int frame_width = int(video.get(cv::CAP_PROP_FRAME_WIDTH));
+		int frame_height = int(video.get(cv::CAP_PROP_FRAME_HEIGHT));
+		fps = video.get(cv::CAP_PROP_FPS);
+
+		// output decoding
+		cv::VideoWriter writer("../result.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, cv::Size(frame_width, frame_height));
+		// inference
+        int fcount = 0;
+		cv::Mat img;
+		for(int f = 0; f < frame_num-1; f++)
+		{
+            fcount++;
+            std::cout << "=============" << std::endl;
+            if (fcount < BATCH_SIZE && f + 1 != frame_num) continue;
+            for (int b = 0; b < fcount; b++) 
+            {
+
+                video>>img;
+                if (img.empty()) continue;
+                cv::Mat pr_img = preprocess_img(img);
+                // image normalization 
+                for (int i = 0; i < INPUT_H * INPUT_W; i++) 
+                {
+                    data[b * 3 * INPUT_H * INPUT_W + i] = pr_img.at<cv::Vec3b>(i)[2] / 255.0;
+                    data[b * 3 * INPUT_H * INPUT_W + i + INPUT_H * INPUT_W] = pr_img.at<cv::Vec3b>(i)[1] / 255.0;
+                    data[b * 3 * INPUT_H * INPUT_W + i + 2 * INPUT_H * INPUT_W] = pr_img.at<cv::Vec3b>(i)[0] / 255.0;
+                }
+            }
+
+            // Run inference
+            auto start = std::chrono::system_clock::now();
+            doInference(*context, data, prob, BATCH_SIZE);
+            auto end = std::chrono::system_clock::now();
+            std::cout << "Inference time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+
+            std::vector<std::vector<Yolo::Detection>> batch_res(fcount);
+            // nms
+            for (int b = 0; b < fcount; b++) 
+            {
+                auto& res = batch_res[b];
+                nms(res, &prob[b * OUTPUT_SIZE], CONF_THRESH, NMS_THRESH);
+            }
+            for (int b = 0; b < fcount; b++)
+            {
+                auto& res = batch_res[b];
+                // inference results
+    	        std::cout << "object number is: " << res.size() << std::endl;
+                // write image to file 
+    	        for (size_t j = 0; j < res.size(); j++)
+                {
+                    int class_idx = (int)res[j].class_id;
+                    cv::Rect r = get_rect(img, res[j].bbox);
+                    cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+                    cv::putText(img, categories[class_idx], cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+                            
+                }
+    	        writer << img;
+            }
+            fcount = 0;
+		}
+		
+	}
+
+    // Destroy the engine
+    context->destroy();
+    engine->destroy();
+    runtime->destroy();
+
+    return 0;
+}
